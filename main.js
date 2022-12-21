1084ed220d41b4d66b9412e6fe2b4ee60ac7bc75
Cristhian Salas <cristhian.sm91@gmail.com>
2022-12-20 21:04:09 -0600

    [ADD] elements  in js

---
+++ main.js
@@ -0,0 +1,9 @@
+// set date in footer
+const date = document.getElementById('date');
+date.innerHTML = new Date().getFullYear();
+
+// get DOM elements
+
+const navToggle = document.querySelector('.button__menu-mobile');
+const linksContainer = document.querySelector('.link-container');
+const links = document.querySelector('.links');
